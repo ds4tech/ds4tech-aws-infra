8c6147726335d2e19a65a48873b7e327684d95e1
Mateusz Szymczyk <mateusz.szymczyk@tuispain.com>
2022-12-28 14:46:45 +0100

    add alb and network

---
+++ infra/alb.tf
@@ -0,0 +1,82 @@
+module "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 8.0"
+
+  name = "my-alb"
+
+  load_balancer_type = "application"
+
+  vpc_id             = module.vpc.vpc_id
+  subnets            = module.vpc.public_subnets
+  security_groups    = [module.security_group_ec2.security_group_id, module.security_group_rds.security_group_id]
+
+  access_logs = {
+    bucket = module.s3_bucket_for_logs.s3_bucket_id
+  }
+
+#   target_groups = [
+#     {
+#       name_prefix      = "pref-"
+#       backend_protocol = "HTTP"
+#       backend_port     = 80
+#       target_type      = "instance"
+#       targets = {
+#         my_target = {
+#           target_id = "i-0123456789abcdefg"
+#           port = 80
+#         }
+#         my_other_target = {
+#           target_id = "i-a1b2c3d4e5f6g7h8i"
+#           port = 8080
+#         }
+#       }
+#     }
+#   ]
+  target_groups = [
+    {
+      name_prefix          = "h1"
+      backend_protocol     = "HTTP"
+      backend_port         = 80
+      target_type          = "instance"
+      deregistration_delay = 10
+      health_check = {
+        enabled             = true
+        interval            = 30
+        path                = "/healthz"
+        port                = "traffic-port"
+        healthy_threshold   = 3
+        unhealthy_threshold = 3
+        timeout             = 6
+        protocol            = "HTTP"
+        matcher             = "200-399"
+      }
+      protocol_version = "HTTP1"
+      targets = {
+        my_ec2 = {
+          target_id = module.ec2_instance.id
+          port      = 80
+        },
+      }
+      tags = {
+        InstanceTargetGroupTag = "baz"
+      }
+    },
+  ]
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol    = "HTTP"
+      action_type = "fixed-response"
+      fixed_response = {
+        content_type = "text/plain"
+        message_body = "Fixed message"
+        status_code  = "200"
+      }
+    }
+  ]
+
+  tags = {
+    Environment = "dev"
+  }
+}
