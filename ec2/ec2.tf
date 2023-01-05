fa687160a0e655b39dabbc1dfbd695cb803446b2
Mateusz Szymczyk <ds4tech@gmail.com>
2023-01-05 12:06:20 +0100

    read security groups as datasources

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,14 +1,3 @@
-// Create aws_ami filter to pick up the ami available in your region
-data "aws_ami" "amazon-linux-2" {
-  most_recent = true
-  owners      = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm*"]
-  }
-}
-
 module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "4.2.1"
@@ -19,19 +8,19 @@ module "ec2_instance" {
   instance_type          = "t2.micro"
   key_name               = "11.22-kp-aws"
   monitoring             = true
-  # associate_public_ip_address = false
-  vpc_security_group_ids = [var.security_group_id]
-  subnet_id              = "subnet-04f8cb5655bafd47e"#data.aws_subnet.public_1a
+  associate_public_ip_address = false
+  vpc_security_group_ids = data.aws_security_groups.selected.ids
+  subnet_id              = data.aws_subnets.public.ids[0]
 
   user_data_base64            = base64encode(local.user_data)
 
   tags = local.tags
 }
 
-module "eip" {
-  source  = "jfcantu/eip/aws"
-  version = "1.0.1"
-  instance = module.ec2_instance.id
+# module "eip" {
+#   source  = "jfcantu/eip/aws"
+#   version = "1.0.1"
+#   instance = module.ec2_instance.id
   # associate_with_private_ip = 
   # network_interface = 
-}
+# }
--- ec2/outputs.tf
+++ ec2/outputs.tf
@@ -8,3 +8,8 @@ output "instance_IP" {
   description = "EC2 instance ip address"
   value       = module.ec2_instance.public_ip
 }
+
+output "VPC_public_IP_address" {
+  description = "existing public ip address"
+  value       = data.aws_eip.vpc_ip.public_ip
+}
