9e34cbf275c98d2e347dde7e1046d66e1a165dff
Mateusz Szymczyk <ds4tech@gmail.com>
2023-01-06 12:42:29 +0100

    add data file to read from existing infra

---
+++ infra/dev/data.tf
@@ -0,0 +1,52 @@
+// Create aws_ami filter to pick up the ami available in your region
+data "aws_ami" "amazon-linux-2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+}
+// select existing VPC
+data "aws_vpc" "selected" {
+  filter {
+    name   = "tag:Name"
+    values = ["education-vpc"]
+  }
+}
+
+// select existing public subnet
+data "aws_subnets" "public" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.selected.id]
+  }
+  filter {
+    name   = "tag:Name"
+    values = ["*public*"]
+  }
+}
+
+data "aws_security_groups" "selected" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.selected.id]
+  }
+  filter {
+    name   = "tag:Purpose"
+    values = ["EC2 instance"]
+  }
+}
+
+data "aws_eip" "vpc_ip" {
+  tags = {
+    Name = "education-vpc-eu-central-1a"
+  }
+}
+# data "aws_eip" "by_filter" {
+#   filter {
+#     name   = "tag:Name"
+#     values = ["exampleNameTagValue"]
+#   }
+# }
--- infra/dev/main.tf
+++ infra/dev/main.tf
@@ -14,3 +14,12 @@ module "ec2_instance" {
 
   region = local.region
 }
+
+module "3rd_party" {
+  source  = "../../modules/3rd_party"
+
+  region = local.region
+  vpc_id             = module.vpc.vpc_id
+  subnets            = module.vpc.public_subnets
+  security_groups    = [module.security_group_ec2.security_group_id]
+}
